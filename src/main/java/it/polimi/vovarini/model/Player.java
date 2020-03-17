1000e5d811f422cd7daee2ccd3ef80964c3ab0c0
MV511 <mattia.valassi@mail.polimi.it>
2020-03-17 18:39:51 +0100

    added godcard specific classes, other

--- src/main/java/it/polimi/vovarini/model/Player.java
+++ src/main/java/it/polimi/vovarini/model/Player.java
@@ -1,4 +1,42 @@
 package it.polimi.vovarini.model;
 
+import it.polimi.vovarini.model.board.Worker;
+import it.polimi.vovarini.model.board.items.Sex;
+import it.polimi.vovarini.model.godcards.GodCard;
+
+
 public class Player {
+    private int movementsLeft;
+    private boolean hasMoved;
+    private boolean hasBuilt;
+    private Worker currentWorker;
+    private Worker workers[];
+    private GodCard card;
+    private String nickname;
+
+
+    public Player (GodCard assignedCard, String nickname){
+
+        //che valore dovrei mettere qui di default?
+        this.movementsLeft = 0;
+        this.hasMoved = false;
+        this.hasBuilt = false;
+        this.workers = new Worker[2];
+        this.workers[0] = new Worker(/*Sex.Female*/);
+        this.workers[1] = new Worker(/*Sex.Male*/);
+        this.currentWorker = this.workers[0];
+        this.card = assignedCard;
+        //possibile che serva un controllo da parte di Game per vedere che il nick non sia già usato?
+        this.nickname = nickname;
+    }
+
+    //permette al giocatore di selezionare quali dei due worker vuole utilizzare
+    public void chooseWorker (int i){
+        //serve sicuramente un controllo sull'indice, ed eventualmente un'eccezione
+        this.currentWorker = this.workers[i];
+    }
+
+
+
+
 }
---
+++ src/main/java/it/polimi/vovarini/model/godcards/GodName.java
@@ -0,0 +1,5 @@
+package it.polimi.vovarini.model.godcards;
+
+public enum GodName {
+    Apollo, Artemis, Athena, Atlas, Demeter, Hephaestus, Minotaur, Pan, Prometheus;
+}
