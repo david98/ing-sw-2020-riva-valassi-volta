c6e9f8328b308a2b09420e1f1bac4538ad3f446e
marcoriva <marco20.riva@mail.polimi.it>
2020-03-27 13:06:34 +0100

    PlayerTests on constructor and bug #17 fixed

---
+++ src/test/java/it/polimi/vovarini/model/PlayerTests.java
@@ -0,0 +1,27 @@
+package it.polimi.vovarini.model;
+
+import it.polimi.vovarini.model.board.items.Worker;
+import it.polimi.vovarini.model.godcards.GodCard;
+import it.polimi.vovarini.model.godcards.Nobody;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerTests {
+
+    @Test
+    @DisplayName("Test that a Player can be instantiated correctly")
+    void playerCreation() {
+        Game game = new Game(2);
+        GodCard godCard = new Nobody(game);
+        String nickname = "Guest";
+
+        Player player = new Player(game, godCard, nickname);
+
+        assertEquals("M", player.getCurrentWorker().toString());
+        assertEquals("F", player.getOtherWorker().toString());
+        assertEquals(godCard, player.getGodCard());
+        assertEquals (nickname, player.getNickname());
+    }
+}
