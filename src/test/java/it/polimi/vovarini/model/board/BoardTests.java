9e04b8259101194e73305752a822ba1e3a9b546e
MV511 <mattia.valassi@mail.polimi.it>
2020-03-29 15:58:36 +0200

    aggiunto test adjPoints. Serve sapere se ho la versione corretta del metodo

--- src/test/java/it/polimi/vovarini/model/board/BoardTests.java
+++ src/test/java/it/polimi/vovarini/model/board/BoardTests.java
@@ -1,9 +1,14 @@
 package it.polimi.vovarini.model.board;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import it.polimi.vovarini.model.Point;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class BoardTests {
   @Test
@@ -11,5 +16,17 @@ public class BoardTests {
   void boardCreation() {
     Board board = new Board(Board.DEFAULT_SIZE);
     assertEquals(Board.DEFAULT_SIZE, board.getSize());
+  }
+
+  @Test
+  @DisplayName("Test that every points valuated as valid by the method is adjacent")
+  void adjacentPoints(Point current){
+
+    Board board = new Board(Board.DEFAULT_SIZE);
+    List<Point> adjPointsList = board.getAdjacentPositions(current);
+    for (Point point : adjPointsList){
+      int distance = Math.abs( (current.getX() + current.getY()) - (point.getX() + point.getY()) );
+      assertTrue(distance < 1 && !point.equals(current) && point.getX() >= 0 && point.getY() >= 0 && point.getX() <= Board.DEFAULT_SIZE && point.getY() <= Board.DEFAULT_SIZE );
+    }
   }
 }
