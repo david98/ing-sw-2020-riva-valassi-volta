9007b2633813da88a158d61254eef4a308c0ac17
Davide Volta <davidevolta98@gmail.com>
2020-03-30 18:56:23 +0200

    first abstract of controller

---
+++ src/main/java/it/polimi/vovarini/controller/Controller.java
@@ -0,0 +1,20 @@
+package it.polimi.vovarini.controller;
+
+import it.polimi.vovarini.model.Game;
+import it.polimi.vovarini.model.Point;
+import it.polimi.vovarini.model.board.items.Sex;
+
+public abstract class Controller {
+
+  public abstract void startGame(int numberOfPlayers);
+
+  public abstract void addPlayers(String[] nicknames);
+
+  public abstract void selectWorker(Sex sex);
+
+  public abstract void moveCurrentWorker(Point destination);
+
+  public abstract void buildBlock(Point target);
+
+
+}
