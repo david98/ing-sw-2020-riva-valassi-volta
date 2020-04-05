47e7c5c906ee86771196f7091ef7dd5465fb29be
Davide Volta <davidevolta98@gmail.com>
2020-04-05 16:17:05 +0200

    metodo per Apollo

--- src/main/java/it/polimi/vovarini/model/godcards/GodCardFactory.java
+++ src/main/java/it/polimi/vovarini/model/godcards/GodCardFactory.java
@@ -1,20 +1,47 @@
 package it.polimi.vovarini.model.godcards;
 
 import it.polimi.vovarini.model.Game;
-import it.polimi.vovarini.model.Movement;
 import it.polimi.vovarini.model.Point;
-import it.polimi.vovarini.model.board.Board;
 import it.polimi.vovarini.model.board.BoxEmptyException;
 import it.polimi.vovarini.model.board.InvalidPositionException;
 import it.polimi.vovarini.model.board.ItemNotFoundException;
-import it.polimi.vovarini.model.board.items.Block;
 import it.polimi.vovarini.model.board.items.Item;
 import it.polimi.vovarini.model.board.items.Worker;
 
-import java.util.Arrays;
 import java.util.Stack;
+import java.util.function.BiFunction;
 
 public class GodCardFactory {
+
+  private static BiFunction<Game, Point, Boolean> isPointReachableCanExchangeWithWorker =
+          (Game game, Point point) -> {
+            try {
+              Worker currentWorker = game.getCurrentPlayer().getCurrentWorker();
+              Point currentWorkerPosition = game.getBoard()
+                      .getItemPosition(currentWorker);
+              if (!point.isAdjacent(currentWorkerPosition)){
+                return false;
+              }
+
+              try {
+                Stack<Item> destinationItems = game.getBoard().getItems(point);
+
+                int destinationLevel = destinationItems.size();
+                int currentWorkerLevel = game.getBoard().getItems(currentWorkerPosition).size();
+                return (destinationLevel - currentWorkerLevel <= 1) &&
+                        (currentWorker.canBePlacedOn(destinationItems.peek()) ||
+                                destinationItems.peek().canBeRemoved());
+              } catch (BoxEmptyException ignored){
+                return true;
+              }
+
+            } catch (ItemNotFoundException | InvalidPositionException ignored){
+              System.err.println("This really should never happen...");
+            }
+            return false;
+
+  };
+
   public static GodCard create(GodName name){
     switch (name){
       case Apollo: {
@@ -29,6 +56,7 @@ public class GodCardFactory {
 
   private static GodCard createApollo(){
     GodCard apollo = new GodCard(GodName.Apollo);
+    apollo.isPointReachable = isPointReachableCanExchangeWithWorker;
     return apollo;
   }
 
