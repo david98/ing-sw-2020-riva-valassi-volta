56007665046b2e9bb6c555276ca0d0c4d7493c61
marcoriva <marco20.riva@mail.polimi.it>
2020-04-22 12:21:15 +0200

    refactoring minotaur tests

--- src/test/java/it/polimi/vovarini/model/godcards/MinotaurTests.java
+++ src/test/java/it/polimi/vovarini/model/godcards/MinotaurTests.java
@@ -46,38 +46,32 @@ public class MinotaurTests {
     }
 
     @Test
-    @DisplayName("Test a movement with a GodCard of type Minotaur")
-    public void minotaurMovement() {
-
-        GodCard minotaur = game.getCurrentPlayer().getGodCard();
-        game.getCurrentPlayer().setCurrentSex(Sex.Female);
-        Worker currentWorker = game.getCurrentPlayer().getCurrentWorker();
-        Worker otherWorker = game.getCurrentPlayer().getOtherWorker();
-
-        Player enemysPlayer = game.getPlayers()[1];
-        Worker enemysWorker = enemysPlayer.getCurrentWorker();
-
-        Board board = game.getBoard();
-        Point start = new Point(0,0);
-        Point end = new Point (1,1);
-        Point forcedDestination = new Point(2,2);
-        Movement movement = new Movement(board, start, end);
-        List<Movement> movementList;
+    @DisplayName("Test an invalid movement with a GodCard of type Minotaur")
+    /** status:
+     *  startBox: lv 2 + currentWorker
+     *  endBox: lv 0 + otherWorker of currentPlayer
+     *  forcedDestinationBox: lv 0 + free
+     */
+    public void invalidMovementOtherWorker() {
 
         try {
-            /* invalid move
-             * start: lv 2 + currentWorker
-             * end: lv 0 + otherWorker of currentPlayer
-             * forcedDestination: lv 0 + free
-             */
+            GodCard minotaur = game.getCurrentPlayer().getGodCard();
+            Worker currentWorker = game.getCurrentPlayer().getCurrentWorker();
+            Worker otherWorker = game.getCurrentPlayer().getOtherWorker();
+
+            Board board = game.getBoard();
+            Point start = new Point(0, 0);
+            Point end = new Point(1, 1);
+            Movement movement = new Movement(board, start, end);
+
             board.place(Block.blocks[0], start);
             board.place(Block.blocks[1], start);
             board.place(currentWorker, start);
             board.place(otherWorker, end);
 
-            if(game.validateMove(movement)) {
-                movementList = minotaur.listEffects(movement);
-                for(Movement m : movementList) {
+            if (game.validateMove(movement)) {
+                List<Movement> movementList = minotaur.listEffects(movement);
+                for (Movement m : movementList) {
                     game.performMove(m);
                 }
             }
@@ -86,18 +80,42 @@ public class MinotaurTests {
             assertEquals(currentWorker, board.getBox(start).getItems().peek());
             assertEquals(otherWorker, board.getBox(end).getItems().peek());
 
+        } catch (BoxFullException ignored) {
+        } catch (BoxEmptyException ignored) {
+        } catch (InvalidPositionException ignored) {
+        }
+    }
 
-            /* invalid move
-             * start: lv 2 + currentWorker
-             * end: lv 0 + enemy's Worker
-             * forcedDestination: lv 0 + otherWorker of currentPlayer
-             */
-            board.remove(end);
+    @Test
+    @DisplayName("Test an invalid movement with a GodCard of type Minotaur")
+    /** status
+     *  startBox: lv 2 + currentWorker
+     *  endBox: lv 0 + enemy's Worker
+     *  forcedDestinationBox: lv 0 + otherWorker of currentPlayer
+     */
+    public void invalidMovementForcedDestinationOccupied() {
+
+        try {
+            GodCard minotaur = game.getCurrentPlayer().getGodCard();
+            Worker currentWorker = game.getCurrentPlayer().getCurrentWorker();
+            Worker otherWorker = game.getCurrentPlayer().getOtherWorker();
+            Player enemysPlayer = game.getPlayers()[1];
+            Worker enemysWorker = enemysPlayer.getCurrentWorker();
+
+            Board board = game.getBoard();
+            Point start = new Point(0, 0);
+            Point end = new Point(1, 1);
+            Point forcedDestination = new Point(2,2);
+            Movement movement = new Movement(board, start, end);
+
+            board.place(Block.blocks[0], start);
+            board.place(Block.blocks[1], start);
+            board.place(currentWorker, start);
             board.place(enemysWorker, end);
             board.place(otherWorker, forcedDestination);
 
             if(game.validateMove(movement)) {
-                movementList = minotaur.listEffects(movement);
+                List<Movement> movementList = minotaur.listEffects(movement);
                 for(Movement m : movementList) {
                     game.performMove(m);
                 }
@@ -108,71 +126,45 @@ public class MinotaurTests {
             assertEquals(enemysWorker, board.getBox(end).getItems().peek());
             assertEquals(otherWorker, board.getBox(forcedDestination).getItems().peek());
 
+        } catch (BoxFullException ignored) {
+        } catch (BoxEmptyException ignored) {
+        } catch (InvalidPositionException ignored) {
+        }
+    }
 
-            /* valid move
-             * start: lv 2 + currentWorker
-             * end: lv 0 + enemy's Worker
-             * forcedDestination: lv 3 + free
-             */
-            board.remove(forcedDestination);
-            board.place(Block.blocks[0], forcedDestination);
-            board.place(Block.blocks[1], forcedDestination);
-            board.place(Block.blocks[2], forcedDestination);
+    @Test
+    @DisplayName("Test an invalid movement with a GodCard of type Minotaur")
+    /** status
+     *  startBox: lv 1 + currentWorker
+     *  endBox: lv 3 + enemy's Worker
+     *  forcedDestinationBox: lv 3 + free
+     */
+    public void invalidMovementLevel() {
 
-            assertTrue(Reachability.isPointReachableConditionedExchange(game, end));
-            assertFalse(minotaur.isMovementWinning(movement));
+        try {
+            GodCard minotaur = game.getCurrentPlayer().getGodCard();
+            Worker currentWorker = game.getCurrentPlayer().getCurrentWorker();
+            Player enemysPlayer = game.getPlayers()[1];
+            Worker enemysWorker = enemysPlayer.getCurrentWorker();
 
-            if(game.validateMove(movement)) {
-                movementList = minotaur.listEffects(movement);
-                for(Movement m : movementList) {
-                    game.performMove(m);
-                }
-            }
+            Board board = game.getBoard();
+            Point start = new Point(0, 0);
+            Point end = new Point(1, 1);
+            Point forcedDestination = new Point(2,2);
+            Movement movement = new Movement(board, start, end);
 
-            assertEquals(currentWorker, board.getBox(end).getItems().peek());
-            assertEquals(enemysWorker, board.getBox(forcedDestination).getItems().peek());
-
-
-            /* valid & winning move
-             * start: lv 2 + currentWorker
-             * end: lv 3 + enemy's Worker
-             * forcedDestination: lv 3 + free
-             */
-            board.remove(end);
+            board.place(Block.blocks[0], start);
             board.place(currentWorker, start);
             board.place(Block.blocks[0], end);
             board.place(Block.blocks[1], end);
             board.place(Block.blocks[2], end);
-            board.remove(forcedDestination);
             board.place(enemysWorker, end);
-
-            assertTrue(Reachability.isPointReachableConditionedExchange(game, end));
-            assertTrue(minotaur.isMovementWinning(movement));
+            board.place(Block.blocks[0], forcedDestination);
+            board.place(Block.blocks[1], forcedDestination);
+            board.place(Block.blocks[2], forcedDestination);
 
             if(game.validateMove(movement)) {
-                movementList = minotaur.listEffects(movement);
-                for(Movement m : movementList) {
-                    game.performMove(m);
-                }
-            }
-
-            assertEquals(currentWorker, board.getBox(end).getItems().peek());
-            assertEquals(enemysWorker, board.getBox(forcedDestination).getItems().peek());
-
-
-            /* invalid move
-             * start: lv 1 + currentWorker
-             * end: lv 3 + enemy's Worker
-             * forcedDestination: lv 3 + free
-             */
-            board.remove(start);
-            board.remove(end);
-            board.remove(forcedDestination);
-            board.place(currentWorker, start);
-            board.place(enemysWorker, end);
-
-            if(game.validateMove(movement)) {
-                movementList = minotaur.listEffects(movement);
+                List<Movement> movementList = minotaur.listEffects(movement);
                 for(Movement m : movementList) {
                     game.performMove(m);
                 }
@@ -183,8 +175,109 @@ public class MinotaurTests {
             assertEquals(enemysWorker, board.getBox(end).getItems().peek());
 
         } catch (BoxFullException ignored) {
-        } catch (InvalidPositionException ignored) {
         } catch (BoxEmptyException ignored) {
+        } catch (InvalidPositionException ignored) {
+        }
+    }
+
+    @Test
+    @DisplayName("Test a valid movement with a GodCard of type Minotaur")
+    /** status
+     *  startBox: lv 2 + currentWorker
+     *  endBox: lv 0 + enemy's Worker
+     *  forcedDestinationBox: lv 3 + free
+     */
+    public void validMovement() {
+
+        try {
+            GodCard minotaur = game.getCurrentPlayer().getGodCard();
+            Worker currentWorker = game.getCurrentPlayer().getCurrentWorker();
+            Worker otherWorker = game.getCurrentPlayer().getOtherWorker();
+            Player enemysPlayer = game.getPlayers()[1];
+            Worker enemysWorker = enemysPlayer.getCurrentWorker();
+
+            Board board = game.getBoard();
+            Point start = new Point(0, 0);
+            Point end = new Point(1, 1);
+            Point forcedDestination = new Point(2,2);
+            Movement movement = new Movement(board, start, end);
+
+            board.place(Block.blocks[0], start);
+            board.place(Block.blocks[1], start);
+            board.place(currentWorker, start);
+            board.place(enemysWorker, end);
+            board.place(Block.blocks[0], forcedDestination);
+            board.place(Block.blocks[1], forcedDestination);
+            board.place(Block.blocks[2], forcedDestination);
+
+            assertTrue(Reachability.isPointReachableConditionedExchange(game, end));
+            assertFalse(minotaur.isMovementWinning(movement));
+
+            if(game.validateMove(movement)) {
+                List<Movement> movementList = minotaur.listEffects(movement);
+                for(Movement m : movementList) {
+                    game.performMove(m);
+                }
+            }
+
+            assertEquals(currentWorker, board.getBox(end).getItems().peek());
+            assertEquals(enemysWorker, board.getBox(forcedDestination).getItems().peek());
+
+        } catch (BoxFullException ignored) {
+        } catch (BoxEmptyException ignored) {
+        } catch (InvalidPositionException ignored) {
+        }
+    }
+
+    @Test
+    @DisplayName("Test a valid and winning movement with a GodCard of type Minotaur")
+    /** status
+     *  startBox: lv 2 + currentWorker
+     *  endBox: lv 3 + enemy's Worker
+     *  forcedDestinationBox: lv 3 + free
+     */
+    public void validWinningMovement() {
+
+        try {
+            GodCard minotaur = game.getCurrentPlayer().getGodCard();
+            Worker currentWorker = game.getCurrentPlayer().getCurrentWorker();
+            Worker otherWorker = game.getCurrentPlayer().getOtherWorker();
+            Player enemysPlayer = game.getPlayers()[1];
+            Worker enemysWorker = enemysPlayer.getCurrentWorker();
+
+            Board board = game.getBoard();
+            Point start = new Point(0, 0);
+            Point end = new Point(1, 1);
+            Point forcedDestination = new Point(2,2);
+            Movement movement = new Movement(board, start, end);
+
+            board.place(Block.blocks[0], start);
+            board.place(Block.blocks[1], start);
+            board.place(currentWorker, start);
+            board.place(Block.blocks[0], end);
+            board.place(Block.blocks[1], end);
+            board.place(Block.blocks[2], end);
+            board.place(enemysWorker, end);
+            board.place(Block.blocks[0], forcedDestination);
+            board.place(Block.blocks[1], forcedDestination);
+            board.place(Block.blocks[2], forcedDestination);
+
+            assertTrue(Reachability.isPointReachableConditionedExchange(game, end));
+            assertTrue(minotaur.isMovementWinning(movement));
+
+            if(game.validateMove(movement)) {
+                List<Movement> movementList = minotaur.listEffects(movement);
+                for(Movement m : movementList) {
+                    game.performMove(m);
+                }
+            }
+
+            assertEquals(currentWorker, board.getBox(end).getItems().peek());
+            assertEquals(enemysWorker, board.getBox(forcedDestination).getItems().peek());
+
+        } catch (BoxFullException ignored) {
+        } catch (BoxEmptyException ignored) {
+        } catch (InvalidPositionException ignored) {
         }
     }
 }
