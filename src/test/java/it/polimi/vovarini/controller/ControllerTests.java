e2edfc9695892d6d5680a165d0efb2c1c7123a79
MV511 <mattia.valassi@mail.polimi.it>
2020-05-19 10:17:22 +0200

    fixed spawnWorker

--- src/test/java/it/polimi/vovarini/controller/ControllerTests.java
+++ src/test/java/it/polimi/vovarini/controller/ControllerTests.java
@@ -407,7 +407,6 @@ public class ControllerTests {
   }
 
   @Test
-  @Disabled
   @DisplayName("Tests that a correct SpwanWorkerEvent places the currentWorker in the right place, which is the target parameter")
   void spawnWorkerTest() {
 
@@ -470,14 +469,15 @@ public class ControllerTests {
       controller.update(evtWorkerAlreadySpawned);
     });
 
+    game.nextPlayer();
+
     target = new Point(0, 0);
     game.getCurrentPlayer().setCurrentSex(Sex.Female);
     SpawnWorkerEvent evtOverwrittenWorker = new SpawnWorkerEvent(game.getCurrentPlayer(), target);
     assertThrows(OverwrittenWorkerException.class, () -> {
       controller.update(evtOverwrittenWorker);
     });
-
-    game.nextPlayer();
+    
     assertEquals(game.getCurrentPlayer(), game.getPlayers()[1]);
 
     game.getCurrentPlayer().setCurrentSex(Sex.Male);
