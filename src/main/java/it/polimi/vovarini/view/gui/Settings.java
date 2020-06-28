a7a3d33ef36ba701e5f0f05cb684c084340b491b
MV511 <mattia.valassi@mail.polimi.it>
2020-06-28 11:34:21 +0200

    hardcoded descriptions hashmap

--- src/main/java/it/polimi/vovarini/view/gui/Settings.java
+++ src/main/java/it/polimi/vovarini/view/gui/Settings.java
@@ -10,10 +10,30 @@ public class Settings {
 
   public static final HashMap<GodName, Image> godImages = new HashMap<>();
   public static final HashMap<Sex, Image>[] workersImages = new HashMap[3];
+  public static final HashMap<GodName, String> descriptions = new HashMap<>();
   public static final Image[] levelsImages = new Image[4];
   public static final Image bg = new Image(Settings.class.getClassLoader().getResource("img/bg.png").toExternalForm(), true);
   public static final Image victory = new Image(Settings.class.getClassLoader().getResource("img/victory.png").toExternalForm(), true);
   public static final Image loss = new Image(Settings.class.getClassLoader().getResource("img/loss.png").toExternalForm(), true);
+
+  public static final String powers[] = {
+          "Apollo: Quando ti muovi, puoi scambiarti con un lavoratore adiacente.",
+          "Artemis: Puoi spostarti una volta in più, ma non puoi tornare alla casella da dove sei partito.\nPremi SKIP se non vuoi utilizzare il potere.",
+          "Athena: Se nel tuo ultimo turno sei salito di livello, i tuoi avversari non potranno salire di livello.",
+          "Atlas: Puoi costruire una cupola sopra qualsiasi blocco (premendo tasto destro).",
+          "Demeter: Puoi costruire una volta in più, ma non nella stessa casella.\nPremi SKIP se non vuoi utilizzare il potere.",
+          "Hephaestus: Puoi costruire un blocco aggiuntivo (non una cupola) al di sopra della casella su cui hai già costruito.\nPremi SKIP se non vuoi utilizzare il potere.",
+          "Hera: Impedisci all'avversario di vincere se sale al terzo livello su una casella perimetrale.",
+          "Hestia: Puoi costruire una volta in più in qualsiasi casella, a patto che non sia perimetrale.\nPremi SKIP se non vuoi utilizzare il potere.",
+          "Limus: I lavoratori avversari non possono costruire nelle caselle adiacenti ai tuoi lavoratori, a meno che costruiscano una cupola per formare una torre completa.",
+          "Minotaur: Puoi forzare una pedina avversaria adiacente a spostarsi in una direzione libera, prendendo il suo posto. L'avversario è obbligato a spostarsi, indipendentemente dal livello.",
+          "Pan: Puoi vincere anche scendendo di due o più livelli.",
+          "Prometheus: Puoi costruire sia prima che dopo esserti mosso, a patto che tu non salga di livello.\nPremi SKIP se non vuoi utilizzare il potere (effettuerai il normale turno di gioco).",
+          "Triton: Ogni volta che il tuo lavoratore si trova su una casella perimetrale, può spostarsi di nuovo indipendentemente dal livello.\nPremi SKIP se vuoi smettere di utilizzare il potere.",
+          "Zeus: Il tuo lavoratore può costruire nella casella al di sotto di sé, aumentandone il livello.\nPremi SKIP se non vuoi utilizzare il potere.",
+          "Nobody: I tuoi lavoratori sono noiosi, punto."
+
+  };
 
 
   static {
@@ -33,6 +53,13 @@ public class Settings {
     for (int i = 0; i < 4; i++) {
       levelsImages[i] = new Image(Settings.class.getClassLoader().getResource(("img/levels/" + (i + 1) + ".png")).toExternalForm(), true);
     }
+
+    int k = 0;
+    for(GodName godName : GodName.values()){
+      descriptions.put(godName, powers[k]);
+      k++;
+    }
+
   }
 
   public static final String ELECTED_PLAYER_SCENE_FXML = "/fxml/electedPlayerScene.fxml";
@@ -45,6 +72,8 @@ public class Settings {
 
   public static final String DUPLICATE_NICKNAME = "Nickname already exists, type a new one";
   public static final String INVALID_NICKNAME = "Invalid nickname, type a new one";
+
+
 
   public static void load() {
     // needed to load images
