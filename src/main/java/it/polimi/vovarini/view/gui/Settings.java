0b5318f0ed0c8a4fb243a674294de28a265b4d6d
david98 <davidevolta98@gmail.com>
2020-06-30 12:26:11 +0200

    static map for descriptions

--- src/main/java/it/polimi/vovarini/view/gui/Settings.java
+++ src/main/java/it/polimi/vovarini/view/gui/Settings.java
@@ -6,6 +6,7 @@ import javafx.scene.image.Image;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Settings {
 
@@ -16,26 +17,22 @@ public class Settings {
   public static final Image victory = new Image(Settings.class.getClassLoader().getResource("img/victory.png").toExternalForm(), true);
   public static final Image loss = new Image(Settings.class.getClassLoader().getResource("img/loss.png").toExternalForm(), true);
 
-  public static final HashMap<GodName, String> descriptions = new HashMap<>();
-
-  public static final String powers[] = {
-          "Apollo: When moving, you can exchange your worker with one of an opponent, if adjacent.",
-          "Artemis: You can move once more, but you cannot get back to the box you just left.\nPress SKIP if you do not wish to use the power.",
-          "Athena: If in your last turn you moved up a level, your opponents won't be able to until their next turn.",
-          "Atlas: You can build a dome on top of any block (with a right click).\nWith a left click, you build following the standard rules.",
-          "Demeter: You can build once more, but you cannot build on the same box you just built upon. \nPress SKIP if you do not wish to use the power.",
-          "Hephaestus: You can build an additional block (not a dome) on the same box.\nPress SKIP if you do not wish to use the power.",
-          "Hera: Your opponents cannot win if they move from level 2 to level 3 while in a box in the perimeter.",
-          "Hestia: You can build once more on every box, excluding the ones on the perimeter.\nPress SKIP if you do not wish to use the power.",
-          "Limus: Your opponent's workers cannot build in boxes adjacent to yours, unless if they build a dome to complete a tower.",
-          "Minotaur: You can force an adjacent worker owned by an opponent to move in a free direction.\nYou then take its place with your worker.\nThe opponent must move, independently from the level value.",
-          "Pan: You can win also moving down two or more levels.",
-          "Prometheus: You can build before and after moving, but only if you do not move up.\nPress SKIP if you do not wish to use the power (You will play a turn with standard rules).",
-          "Triton: Anytime one of your workers finds itself on a perimeter box, it can move again independently from the level.\nPress SKIP if you wish to stop using the power.",
-          "Zeus: Your workers can build under themselves, upping the level of their box by one.\nPress SKIP if you do not wish to use the power.",
-          "Nobody: Your workers are BOOOORING!"
-
-  };
+  public static final Map<GodName, String> descriptions = Map.ofEntries(
+          Map.entry(GodName.Apollo, "Apollo: When moving, you can exchange your worker with one of an opponent, if adjacent."),
+          Map.entry(GodName.Artemis, "Artemis: You can move once more, but you cannot get back to the box you just left.\nPress SKIP if you do not wish to use the power."),
+          Map.entry(GodName.Athena, "Athena: If in your last turn you moved up a level, your opponents won't be able to until their next turn."),
+          Map.entry(GodName.Atlas, "Atlas: You can build a dome on top of any block (with a right click).\nWith a left click, you build following the standard rules."),
+          Map.entry(GodName.Demeter,"Demeter: You can build once more, but you cannot build on the same box you just built upon. \nPress SKIP if you do not wish to use the power."),
+          Map.entry(GodName.Hephaestus, "Hephaestus: You can build an additional block (not a dome) on the same box.\nPress SKIP if you do not wish to use the power."), Map.entry(GodName.Hera, "Hera: Your opponents cannot win if they move from level 2 to level 3 while in a box in the perimeter."),
+          Map.entry(GodName.Hestia, "Hestia: You can build once more on every box, excluding the ones on the perimeter.\nPress SKIP if you do not wish to use the power."),
+          Map.entry(GodName.Limus, "Limus: Your opponent's workers cannot build in boxes adjacent to yours, unless if they build a dome to complete a tower."),
+          Map.entry(GodName.Minotaur, "Minotaur: You can force an adjacent worker owned by an opponent to move in a free direction.\nYou then take its place with your worker.\nThe opponent must move, independently from the level value."),
+          Map.entry(GodName.Pan, "Pan: You can win also moving down two or more levels."),
+          Map.entry(GodName.Prometheus, "Prometheus: You can build before and after moving, but only if you do not move up.\nPress SKIP if you do not wish to use the power (You will play a turn with standard rules)."),
+          Map.entry(GodName.Triton, "Triton: Anytime one of your workers finds itself on a perimeter box, it can move again independently from the level.\nPress SKIP if you wish to stop using the power."),
+          Map.entry(GodName.Zeus, "Zeus: Your workers can build under themselves, upping the level of their box by one.\nPress SKIP if you do not wish to use the power."),
+          Map.entry(GodName.Nobody, "Nobody: Your workers are BOOOORING!")
+  );
 
   static {
     for (GodName name: GodName.values()) {
@@ -53,14 +50,6 @@ public class Settings {
 
     for (int i = 0; i < 4; i++) {
       levelsImages[i] = new Image(Settings.class.getClassLoader().getResource(("img/levels/" + (i + 1) + ".png")).toExternalForm(), true);
-    }
-
-    GodName[] godNames = Arrays.stream(GodName.values()).filter(name -> name != GodName.Nobody).toArray(GodName[]::new);
-
-    int k = 0;
-    for(GodName godName : GodName.values()){
-      descriptions.put(godName, powers[k]);
-      k++;
     }
 
   }
