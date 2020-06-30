a1fd822f7f06f19a6172247ef161a62026318c41
MV511 <mattia.valassi@mail.polimi.it>
2020-06-30 17:30:51 +0200

    worker javadoc completed

--- src/main/java/it/polimi/vovarini/model/board/items/Worker.java
+++ src/main/java/it/polimi/vovarini/model/board/items/Worker.java
@@ -32,14 +32,27 @@ public class Worker extends Item {
     owner = worker.owner;
   }
 
+  /**
+   * Getter method for the Sex of the worker
+   * @return the Sex of the worker (Male or Female)
+   */
   public Sex getSex() {
     return sex;
   }
 
+  /**
+   * Getter method for the Owner of the worker
+   * @return the instance of the Player who owns the worker
+   */
   public Player getOwner() {
     return owner;
   }
 
+  /**
+   * This method checks if an Item can be placed on a Worker
+   * @param item The item which this object would be placed on.
+   * @return true if the item can be placed on the worker, false otherwise
+   */
   @Override
   public boolean canBePlacedOn(Item item) {
     if (item instanceof Block) {
@@ -50,16 +63,28 @@ public class Worker extends Item {
     }
   }
 
+  /**
+   * This method checks if a worker can be removed from his current spot on the Board
+   * @return always true, as a worker is by definition a removable Item
+   */
   @Override
   public boolean canBeRemoved() {
     return true;
   }
 
+  /**
+   * ToString method for the class Worker
+   * @return a string representing the instance of a Worker
+   */
   @Override
   public String toString() {
     return sex == Sex.Male ? "M" : "F";
   }
 
+  /**
+   * hashCode method for the class Worker
+   * @return an hashcode representing the instance of a Worker
+   */
   @Override
   public int hashCode() {
     return Arrays.hashCode(new Object[]{
@@ -68,6 +93,11 @@ public class Worker extends Item {
     });
   }
 
+  /**
+   * equals method for the class Worker
+   * @param obj an object to compare with the Worker
+   * @return true if obj is an instance of Worker and if it has both sex and owner equal in value to the other worker. False otherwise
+   */
   @Override
   public boolean equals(Object obj) {
     if (obj instanceof Worker) {
