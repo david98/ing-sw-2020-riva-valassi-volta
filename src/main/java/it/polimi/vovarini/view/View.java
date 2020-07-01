7b203c86c2bfe86d4e09a653b3f8fc9fba993896
MV511 <mattia.valassi@mail.polimi.it>
2020-07-01 21:57:43 +0200

    completed on view

--- src/main/java/it/polimi/vovarini/view/View.java
+++ src/main/java/it/polimi/vovarini/view/View.java
@@ -17,11 +17,18 @@ public abstract class View implements EventsForViewListener {
 
   protected ViewData data;
 
+  /**
+   * This method builds an instance of View
+   */
   public View() {
     GameEventManager.bindListeners(this);
     data = new ViewData();
   }
 
+  /**
+   * This methods handles a NewPlayerEvent, adding the player to the data of this View
+   * @param e is the NewPlayerEvent to handle
+   */
   @Override
   public void handle(NewPlayerEvent e) {
     client.setSocketTimeout(0);
@@ -30,6 +37,11 @@ public abstract class View implements EventsForViewListener {
     }
     data.addPlayer(e.getNewPlayer());
   }
+
+  /**
+   * This methods handles a PlayerInfoUpdateEvent, updating the information of the player linked to the event inside the data of this View
+   * @param e is the PlayerInfoUpdateEvent to handle
+   */
 
   @Override
   public void handle(PlayerInfoUpdateEvent e) {
@@ -49,6 +61,11 @@ public abstract class View implements EventsForViewListener {
     }
   }
 
+  /**
+   * This methods handles a GodCardUpdateEvent, updating the information of the GodCard linked to the event inside the data of this View
+   * @param e is the GodCardUpdateEvent to handle
+   */
+
   @Override
   public void handle(GodCardUpdateEvent e) {
     e.getUpdatedCard().setGameData(data);
@@ -65,10 +82,20 @@ public abstract class View implements EventsForViewListener {
     }
   }
 
+  /**
+   * This methods handles a LossEvent, adding the information that the player linked to the event has lost to the data of this View
+   * @param e is the LossEvent to handle
+   */
+
   @Override
   public void handle(LossEvent e) {
     data.removePlayer(e.getLosingPlayer());
   }
+
+  /**
+   * This methods handles a CardAssignmentEvent, adding the assigned GodCard to the player inside the data of this View
+   * @param e is the CardAssignmentEvent to handle
+   */
 
   @Override
   public void handle(CardAssignmentEvent e) {
@@ -83,10 +110,21 @@ public abstract class View implements EventsForViewListener {
     }
   }
 
+  /**
+   * This methods handles a BoardUpdateEvent, updating the board inside the data of this View
+   * @param e is the BoardUpdateEvent to handle
+   */
+
   @Override
   public void handle(BoardUpdateEvent e) {
     data.setBoard(e.getNewBoard());
   }
+
+  /**
+   * This methods handles a GodSelectionStartEvent, notifying what player is the one that must select the GodCards to play in the game
+   * inside the data of this View
+   * @param e is the GodSelectionStartEvent to handle
+   */
 
   @Override
   public void handle(GodSelectionStartEvent e) {
@@ -104,6 +142,11 @@ public abstract class View implements EventsForViewListener {
     }
     data.setCurrentPlayer(e.getElectedPlayer());
   }
+
+  /**
+   * This methods handles a FirstPlayerEvent, setting the client's timeout properly
+   * @param e is the FirstPlayerEvent to handle
+   */
 
   @Override
   public void handle(FirstPlayerEvent e) {
